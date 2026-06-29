a99556950c5524f28ece5ad96adf816c81094e6e
Alessandro <aless@helix.dev>
2026-06-29 14:08:13 -0400

    feat: add DevlogEntry type

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface DevlogEntry {
+  id: string;
+  timestamp: string;
+  message: string;
+  tag: string;
+}
+
+export interface EntryStore {
+  version: number;
+  entries: DevlogEntry[];
+}
